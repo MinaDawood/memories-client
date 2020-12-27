fa4d800416beb4ad6417051d8b978cd5e658bcca
minadawood <mina.ziden@hotmail.com>
2020-12-27 12:21:53 +0200

    End main func

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,3 +1,10 @@
+import {
+  CREATE,
+  UPDATE,
+  DELETE,
+  FETCH_ALL,
+  LIKE,
+} from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
@@ -5,7 +12,7 @@ export const getPosts = () => async dispatch => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({
-      type: 'FETCH_ALL',
+      type: FETCH_ALL,
       payload: data,
     });
   } catch (error) {
@@ -18,10 +25,48 @@ export const createPost = post => async dispatch => {
     const { data } = await api.createPost(post);
 
     dispatch({
-      type: 'CREATE',
+      type: CREATE,
       payload: data,
     });
   } catch (error) {
     console.log(error.message);
+  }
+};
+
+export const updatePost = (id, post) => async dispatch => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({
+      type: UPDATE,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = id => async dispatch => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({
+      type: DELETE,
+      payload: id,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = id => async dispatch => {
+  try {
+    const { data } = await api.likePost(id);
+    dispatch({
+      type: UPDATE,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
-const Form = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector(state => state.posts);
   const classes = useStyles();
 
@@ -20,11 +20,11 @@ const Form = () => {
     >
       {posts.map(post => (
         <Grid key={post.id} item xs={12} sm={6}>
-          <Post post={post} />
+          <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
     </Grid>
   );
 };
 
-export default Form;
+export default Posts;
